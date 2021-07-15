50fa19d8b2f68e3d83fc567f78e7ac6bd0f4651c
Elías Leguizamón <elias.leguizamon1997@gmail.com>
2021-07-15 15:16:40 -0300

    Fix env PORT variable

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
 async function main() {
-    await app.listen(process.env.PORT, () =>{
+    await app.listen(process.env.PORT || 5000, () =>{
         console.log('====================================');
         console.log(`App Running on http://localhost:${process.env.PORT}`);
         console.log('====================================');
